3ca6ee5fc3df09cd1e9dc2c3bcadbc3aadf45f8d
NyeongC <cjfsud23@naver.com>
2025-03-14 17:04:26 +0900

    헤더 경로 추가

--- vue-project/src/components/HeaderComponet.vue
+++ vue-project/src/components/HeaderComponet.vue
@@ -5,16 +5,16 @@
             <v-row alian="center">
                 <v-col class="d-flex justify-start">
                     <v-btn :to="{path:'/member/list'}">회원목록</v-btn>
-                    <v-btn :to="{path:'groupchatting/list'}">채팅방목록</v-btn>
+                    <v-btn :to="{path:'/groupchatting/list'}">채팅방목록</v-btn>
                 </v-col>
                 <v-col class="text-center">
                     <v-btn :to="{path: '/'}">chat서비스</v-btn>
                 </v-col>
                 <v-col class="d-flex justify-end">
-                    <v-btn v-if="isLogin">MyChatPage</v-btn>
-                    <v-btn v-if="!isLogin">회원가입</v-btn>
-                    <v-btn v-if="!isLogin">로그인</v-btn>
-                    <v-btn v-if="isLogin">로그아웃</v-btn>
+                    <v-btn v-if="isLogin" :to="{path:'/my/char/page'}">MyChatPage</v-btn>
+                    <v-btn v-if="!isLogin" :to="{path:'/member/create'}">회원가입</v-btn>
+                    <v-btn v-if="!isLogin" :to="{path:'/login'}">로그인</v-btn>
+                    <v-btn v-if="isLogin" @click="doLogout">로그아웃</v-btn>
                 </v-col>
             </v-row>
         </v-container>
@@ -38,7 +38,10 @@ export default{
         }
     }, 
     methods: {
-
+        doLogout() {
+            localStorage.clear();
+            window.location.reload();
+        }
     }
 }
 
