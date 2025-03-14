9c95360781fdaff2c4367bd3bc20b88e26b52781
NyeongC <cjfsud23@naver.com>
2025-03-14 18:25:01 +0900

    태그 정리

--- vue-project/src/views/MemberCreate.vue
+++ vue-project/src/views/MemberCreate.vue
@@ -4,31 +4,32 @@
             <v-col cols="12" sm="4" md="6">
                 <v-card>
                     <v-card-title class="text-h5 text-center">회원가입</v-card-title>
+                    <v-card-text>
+                        <v-form @submit.prevent="memberCreate">
+                            <v-text-field
+                                label="이름"
+                                v-model="name"
+                                required
+                            ></v-text-field>
+
+                            <v-text-field
+                                label="email"
+                                v-model="email"
+                                type="email"
+                                required
+                            ></v-text-field>
+
+                            <v-text-field
+                                label="password"
+                                v-model="password"
+                                type="password"
+                                required
+                            ></v-text-field>
+                            <v-btn type="submit" color="primary" block>등록</v-btn>
+                        </v-form>
+                    </v-card-text>
                 </v-card>
-                <v-card-text>
-                    <v-form @submit.prevent="memberCreate">
-                        <v-text-field
-                            label="이름"
-                            v-model="name"
-                            required
-                        ></v-text-field>
-
-                        <v-text-field
-                            label="email"
-                            v-model="email"
-                            type="email"
-                            required
-                        ></v-text-field>
-
-                        <v-text-field
-                            label="password"
-                            v-model="password"
-                            type="password"
-                            required
-                        ></v-text-field>
-                        <v-btn type="submit" color="primary" block>등록</v-btn>
-                    </v-form>
-                </v-card-text>
+                
             </v-col>
         </v-row>
     </v-container>
