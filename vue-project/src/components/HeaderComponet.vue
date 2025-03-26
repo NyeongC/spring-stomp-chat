2c92b097c1715f18118b6f1d2767b1620265c768
NyeongC <cjfsud23@naver.com>
2025-03-26 13:10:06 +0900

    메시지읽음처리, 채팅방나가기(화면)

--- vue-project/src/components/HeaderComponet.vue
+++ vue-project/src/components/HeaderComponet.vue
@@ -11,7 +11,7 @@
                     <v-btn :to="{path: '/'}">chat서비스</v-btn>
                 </v-col>
                 <v-col class="d-flex justify-end">
-                    <v-btn v-if="isLogin" :to="{path:'/my/char/page'}">MyChatPage</v-btn>
+                    <v-btn v-if="isLogin" :to="{path:'/my/chat/page'}">MyChatPage</v-btn>
                     <v-btn v-if="!isLogin" :to="{path:'/member/create'}">회원가입</v-btn>
                     <v-btn v-if="!isLogin" :to="{path:'/login'}">로그인</v-btn>
                     <v-btn v-if="isLogin" @click="doLogout">로그아웃</v-btn>
